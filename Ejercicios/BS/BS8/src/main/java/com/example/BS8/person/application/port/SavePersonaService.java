281a1826c5d695cf5cffe3c1921496c54ccdba26
GuillermoRamon <91343468+GuillermoRamon@users.noreply.github.com>
2022-06-20 13:35:48 +0200

    update

--- Ejercicios/BS/BS8/src/main/java/com/example/BS8/person/application/port/SavePersonaService.java
+++
@@ -1,9 +0,0 @@
-package com.example.BS8.person.application.port;
-
-
-import com.example.BS8.person.domain.Persona;
-
-public interface SavePersonaService {
-    public Persona addPerson(Persona persona) throws Exception;
-
-}
